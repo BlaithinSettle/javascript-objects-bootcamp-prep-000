c3a35620e033cf16b5e39ef2bb25b6e5eb7d989b
Blaithin Settle <blaithin.settle@yahoo.com>
2018-08-27 06:45:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(obj, key, value){
     obj[key]=value 
     return obj
 }
-function removeFromPlaylist(playlist, artistName){
-    delete playlist.artistName;
+function removeFromPlaylist(obj, artistName){
+    delete playlist['artistName'];
   
 }
